690fcd00c293ee03face9c6095f1a2dec84b6c72
Hydrocallis <makoto.turu@gmail.com>
2022-12-01 18:57:34 +0900

    ver 034

--- __init__.py
+++ __init__.py
@@ -11,7 +11,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
@@ -21,7 +20,7 @@
 bl_info = {
     "name": "Intellisense",
     "author": "Mackraken, tintwotin, Hydrocallis",
-    "version": (0, 3, 3),
+    "version": (0, 3, 4),
     "blender": (3, 3, 1),
     "location": " Text Editor in Scripting tab> Ctrl + Shift + Space, Edit and Context menus,Ctrl + Shift + ENTER, send console",
     "description": "Adds intellisense to the Text Editor",
@@ -55,10 +54,12 @@ try:
 except ModuleNotFoundError:
     from bl_console_utils.autocomplete import intellisense
     
+    
 from console_python import get_console
 
 
 def complete(context):
+    righttsliceline =""
 
     sc = context.space_data
     try:
@@ -71,24 +72,36 @@ def complete(context):
                 break
 
         console = get_console(hash(region))[0]
+        # print('###',console)
 
         line = text.current_line.body
         cursor = text.current_character
+        current_position =bpy.context.space_data.text.current_character
+        # ここでカーソルより左側のみ抽出する必要がある
+        # leftsliceline = line[:current_position]
+        righttsliceline = line[current_position:]
+        # print('###line',line)
 
         result = intellisense.expand(line, cursor, console.locals)
+        # print('###result', )
+        # pprint.pprint(result)
     except AttributeError:
         result=""
-
+        
     return result
 
 
 def make_enumlists(self,context):
+    
     inte_lists=[]
+    # options   は検索結果の候補の中身
     options = complete(bpy.context)
-    options = options[2].split("\n")
+    # print('###option',options)
+    if options != "":
+        options = options[2].split("\n")
 
-    while("" in options) :
-        options.remove("")
+        while("" in options) :
+            options.remove("")
 
     att = False
 
@@ -179,7 +192,7 @@ def text_selection(context):
         endlineincex= txt.select_end_line_index
         # If only one line is selected
         if sline == endline: return txt.lines[endlineincex].body
-        print('###1',txt.lines[endlineincex].body)
+        # print('###1',txt.lines[endlineincex].body)
         rec = 0
         for i, l in enumerate(txt.lines):
             i=i+1
@@ -258,12 +271,12 @@ class TEXT_AP_intellisense_AddonPreferences(AddonPreferences):
             for kmi_con in km.keymap_items: 
                 # print('###kmi_con',kmi_con)  # key map all set     
                 if kmi_add.idname == kmi_con.idname:
-                    # print('###kmi_add.idname',kmi_add.idname)  # The "keymap name of the corresponding add-on" will be displayed.   
+                    # print('###kmi_add.idname',kmi_add.idname)     
 
                     if not kmi_con.id in old_id_l:
                         kmi = kmi_con 
                         old_id_l.append(kmi_con.id) 
-                        # print('###old_id_l',old_id_l)  # keymaps di number   
+                        # print('###old_id_l',old_id_l)     
                         break 
             try:
                 if kmi:
@@ -289,7 +302,7 @@ class TEXT_PG_intellisense_PropertyGroup(PropertyGroup):
 
 class TEXT_OT_intellisense_send_text(Operator):
     #'''Tooltip'''
-    bl_idname = "text.intellioptions_search_send_text"
+    bl_idname = "text.intellioptions_send_text"
     bl_label = "line send text"
 
     @classmethod
@@ -310,7 +323,7 @@ class TEXT_OT_intellisense_send_text(Operator):
 
 class TEXT_OT_intellisense_send_console(Operator):
     #'''Tooltip'''
-    bl_idname = "text.intellioptions_search_send_console"
+    bl_idname = "text.intellioptions_send_console"
     bl_label = "line send console"
 
 
@@ -330,59 +343,17 @@ class TEXT_OT_intellisense_send_console(Operator):
         return {'FINISHED'}
 
     
-class TEXT_OT_intellisense_options(Operator):
+class TEXT_OT_intellisense_search(Operator):
     '''Options'''
-    bl_idname = "text.intellioptions"
-    bl_label = "Intellisense Options"
-
-    text: bpy.props.StringProperty()
+    bl_idname = "text.intellisense_search"
+    bl_label = "Intellisense Search"
+    bl_property = 'iputtext'
 
     @classmethod
     def poll(cls, context):
-        return context.active_object is not None
+        if len(bpy.data.texts) != 0:
+            return True 
 
-    def execute(self, context):
-        sc = context.space_data
-        text = sc.text
-
-        comp = self.text
-        line = text.current_line.body
-
-        lline = len(line)
-        lcomp = len(comp)
-
-        #intersect text
-        intersect = [-1, -1]
-
-        for i in range(lcomp):
-            val1 = comp[0:i + 1]
-
-            for j in range(lline):
-                val2 = line[lline - j - 1::]
-                #print("	",j, val2)
-
-                if val1 == val2:
-                    intersect = [i, j]
-                    break
-
-        comp = comp.strip()
-        if intersect[0] > -1:
-            newline = line[0:lline - intersect[1] - 1] + comp
-        else:
-            newline = line + comp
-        #print(newline)
-        text.current_line.body = newline
-
-        bpy.ops.text.move(type='LINE_END')
-
-        return {'FINISHED'}
-
-
-class TEXT_OT_intellisense_options_search(Operator):
-    '''Options'''
-    bl_idname = "text.intellioptions_search"
-    bl_label = "Intellisense Options"
-    bl_property = 'iputtext'
 
         
     iputtext:bpy.props.EnumProperty(name="test", items=make_enumlists)
@@ -397,9 +368,52 @@ class TEXT_OT_intellisense_options_search(Operator):
             pass
         # If it is in the text, the process is executed   
         elif text.current_character > 0:
+            leftbodyline=text.current_line.body[:text.current_character]
+            righttbodyline=text.current_line.body[text.current_character:]
+            # print("###leftbodyline",leftbodyline)
+            # print("###righttbodyline",righttbodyline)
+            # コンソールに送るために右端を削除してあげないといけない
+            text.current_line.body = leftbodyline
             result = complete(context)
-            text.current_line.body = result[0]
-            bpy.ops.text.move(type='LINE_END')
+            # 右端削除を元に戻す
+            if result[0][-1] ==".":
+                # print('###a',)
+                # .が帰ってきた時
+                text.current_line.body = result[0] + righttbodyline
+
+            if result != "":
+                # print('###b',)
+                if result[2] != "":
+                    if result[2].split("\n")[0][-1] =="]":
+
+                        text.current_line.body = result[0]+ righttbodyline
+
+                        text.current_character = len(result[0])
+                        text.select_end_character = len(result[0])
+                    # 候補が２つの場合（.も含む)
+                    else:
+                        # print('###c',)
+                        text.current_line.body = result[0] + righttbodyline
+                        text.current_character = len(result[0])
+                        text.select_end_character = len(result[0])
+                
+             # 候補が２つの場合（.も含む)
+
+            else:
+                # print('###c',)
+                text.current_line.body = leftbodyline + righttbodyline
+
+            if result[2] == '':
+                # この場合はドッドになる
+                if result[0][-1] ==".":
+                    bpy.ops.text.move(type='NEXT_CHARACTER')
+                # fromなどの処理
+                text.current_line.body = result[0] + righttbodyline
+                text.current_character = len(result[0])
+                text.select_end_character = len(result[0])
+            # print('###result',result)
+            
+            # リザルトに入ってる場合
             if result[2] != '':
                 wm = context.window_manager
                 wm.invoke_search_popup(self)
@@ -407,12 +421,10 @@ class TEXT_OT_intellisense_options_search(Operator):
 
         return {'FINISHED'}
 
-    @classmethod
-    def poll(cls, context):
-        
-        return context.active_object is not None
 
     def execute(self, context):
+        bpy.ops.ed.undo_push() 
+
         sc = context.space_data
         text = sc.text
 
@@ -437,41 +449,25 @@ class TEXT_OT_intellisense_options_search(Operator):
                     break
 
         comp = comp.strip()
+        # print('###',intersect)
         if intersect[0] > -1:
-            newline = line[0:lline - intersect[1] - 1] + comp
+            # newline = line[0:lline - intersect[1] - 1] + comp + line[text.current_character:]
+            newline = line[:text.current_character] + comp + line[text.current_character:]
+            # print('###comp',comp)
+            # print('###a',intersect)
+
+
         else:
-            newline = line + comp
+            newline = line[:text.current_character] + comp + line[text.current_character:]
+            # print('###b',)
+            # print('###comp2',comp)
         #print(newline)
         text.current_line.body = newline
 
-        bpy.ops.text.move(type='LINE_END')
+        for textcount in range(len(comp)):
+            bpy.ops.text.move(type='NEXT_CHARACTER')
+        
 
-        return {'FINISHED'}
-
-
-class TEXT_OT_Intellisense(Operator):
-    '''Text Editor Intellisense'''
-    bl_idname = "text.intellisense"
-    bl_label = "Text Editor Intellisense"
-
-    text = ""
-
-    #   @classmethod
-    #   def poll(cls, context):
-    #	   return context.active_object is not None
-
-    def execute(self, context):
-        sc = context.space_data
-        text = sc.text
-
-        if text.current_character > 0:
-            result = complete(context)
-
-            if result[2] == "":
-                text.current_line.body = result[0]
-                bpy.ops.text.move(type='LINE_END')
-            else:
-                bpy.ops.wm.call_menu(name=TEXT_MT_intellisense_menu.bl_idname)
 
         return {'FINISHED'}
 
@@ -489,51 +485,18 @@ class TEXT_PT_intellisense_panel(Panel):
 
         col = layout.column()
 
-        col.operator("text.intellisense", text="Intellisense")
-        col.menu("TEXT_MT_intellisense_menu", text="Options")
-        col.prop(props,"use_send_console_line_break_bool")
-
-
-class TEXT_MT_intellisense_menu(Menu):
-    '''Intellisense Menu'''
-
-    bl_label = "Intellisense"
-    bl_idname = "TEXT_MT_intellisense_menu"
-
-    text = ""
-
-    def draw(self, context):
-
-        # print(make_enumlists(context))
-
-        layout = self.layout
-        options = complete(context)
-        options = options[2].split("\n")
-
-        while("" in options) :
-            options.remove("")
-
-        att = False
-        # pprint.pprint(options)
-
-        for op in options:
-            if op.find("attribute")>-1:
-                att = True
-            if not att:
-                op = op.lstrip()
-            # print('###op',op)
-            layout.operator("text.intellioptions", text=op).text = op
-
+        col.operator("text.intellisense_search")
+        col.operator("text.intellioptions_send_text")
+        sendconsolebox = col.box()
+        sendconsolebox.operator("text.intellioptions_send_console")
+        sendconsolebox.prop(props,"use_send_console_line_break_bool")
 
 
 classes = [
     TEXT_OT_intellisense_send_text,
     TEXT_OT_intellisense_send_console,
-    TEXT_OT_Intellisense,
-    TEXT_OT_intellisense_options,
-    TEXT_MT_intellisense_menu,
     TEXT_PT_intellisense_panel,
-    TEXT_OT_intellisense_options_search,
+    TEXT_OT_intellisense_search,
     TEXT_PG_intellisense_PropertyGroup,
     TEXT_AP_intellisense_AddonPreferences,
     ]
@@ -553,7 +516,7 @@ def register_keymaps():
         km = wm.keyconfigs.addon.keymaps.new(
             name='Text', space_type='TEXT_EDITOR')
         kmi = km.keymap_items.new(
-            "text.intellioptions_search", #　Only search name is Text Generic. key map disappears when reloading.
+            "text.intellisense_search", #　Only search name is Text Generic. key map disappears when reloading.
             type='SPACE',
             value='PRESS',
             ctrl=True,
@@ -561,11 +524,10 @@ def register_keymaps():
 
         addon_intellisense_keymaps.append((km, kmi))
 
-
         km= wm.keyconfigs.addon.keymaps.new(
             name='Text', space_type='TEXT_EDITOR')
         kmi = km.keymap_items.new(
-            "text.intellioptions_search_send_console",
+            "text.intellioptions_send_console",
             type='RET',
             value='PRESS',
             ctrl=True,
@@ -577,7 +539,7 @@ def register_keymaps():
         km = wm.keyconfigs.addon.keymaps.new(
             name='Text', space_type='TEXT_EDITOR')
         kmi = km.keymap_items.new(
-            "text.intellioptions_search_send_text",
+            "text.intellioptions_send_text",
             type='RET',
             value='PRESS',
             ctrl=True,
@@ -605,9 +567,17 @@ def unregister_keymaps():
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
     if kc:
+        for i , j in addon_intellisense_keymaps:
+            # print('###i j ',i.keymap_items , j)
+            pass
         for km, kmi in addon_intellisense_keymaps:
-            km.keymap_items.remove(kmi)
+            try:
+                km.keymap_items.remove(kmi)
+            except:
+                # print('###error',kmi)
+                pass
     addon_intellisense_keymaps.clear()
+
 
 
 def unregister():
