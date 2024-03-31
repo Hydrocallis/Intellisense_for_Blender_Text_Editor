d172b58f034f9550344aa0b0edabc42a9a55ab7c
Hydrocallis <makoto.turu@gmail.com>
2024-03-31 14:08:49 +0900

    "ver0.3.7 blender 4.0への対応。 3.6でオブジェクトの列挙型リストが変なのを修正"

--- __init__.py
+++ __init__.py
@@ -20,8 +20,8 @@
 bl_info = {
     "name": "Intellisense",
     "author": "Mackraken, tintwotin, Hydrocallis",
-    "version": (0, 3, 6),
-    "blender": (3, 2, 0),
+    "version": (0, 3, 7),
+    "blender": (3, 6, 0),
     "location": " Text Editor in Scripting tab> Ctrl + Shift + Space, Edit and Context menus,Ctrl + Shift + ENTER, send console",
     "description": "Adds intellisense to the Text Editor",
     "warning": "",
